a4d423014ecc7e435466ee75cf7915c657d324aa
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2023-12-13 14:16:31 -0600

    Final Config Dec 2023

--- config.sh
+++ config.sh
@@ -6,6 +6,7 @@ mkdir Bash-Scripts/Syslog
 mkdir Bash-Scripts/LogFiles
 mkdir Bash-Scripts/Config
 mkdir Bash-Scripts/IpPuller
+mkdir Bash-Scripts/Test
 mkdir Themes
 mkdir Themes/Holiday
 mkdir Themes/Creator
@@ -29,14 +30,17 @@ mv LogScripts-main/IPpuller.sh ~/Bash-Scripts/IpPuller
 mv LogScripts-main/Mid.sh ~/Bash-Scripts/IpPuller
 mv LogScripts-main/SysLogAggregation.sh ~/Bash-Scripts/Syslog
 rm LogScripts-main/README.md
+mv LogScripts-main/Testscript.sh ~/Bash-Scripts/Test
 rmdir LogScripts-main
 mv Kali-Terminal-Themes-2023-main/Christmastheme.sh ~/Themes/Holiday
-mv Kali-Terminal-Themes-2023-main/Halloween.sh ~/Themes/Holiday
+mv Kali-Terminal-Themes-2023-main/Halloweentheme.sh ~/Themes/Holiday
 mv Kali-Terminal-Themes-2023-main/change.term.theme.V1.2.sh ~/Themes/Creator
 rm Kali-Terminal-Themes-2023-main/README.md
 rmdir Kali-Terminal-Themes-2023-main/
 rm Kali-Terminal-Themes-2023-main.zip
 rm LogScripts-main.zip
+cd ~
+mv config.sh /Bash-Scripts/Config
 sudo apt-get update
 sudo apt-get terminator
 sudo apt-get figlet
