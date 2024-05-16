bbcc829f31fa9c0b3ebc50a14472eb07de4100d3
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-05-16 18:38:01 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -68,6 +68,8 @@ mv WHGolang-main/type_printer.go ~/Go/fund
 mv WHGolang-main/SimpleScan.go ~/Go/NetTools
 mv WHGolang-main/Concurrentscan.go ~/Go/NetTools
 mv WHGolang-main/toofast.go ~/Go/NetTools
+mv WHGolang-main/worksfast.go ~/Go/NetTools
+mv WHGolang-main/  ~/Go/NetTools
 
 rm WHGolang-main/README.md
 rmdir WHGolang-main
