56eacaf4dfbb2501761c2dca3463ad435c0febfa
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-06-06 17:06:32 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -52,6 +52,12 @@ git clone https://github.com/s0md3v/Photon.git
 cd Photon
 python3 photon.py -h
 cd ~
+mkdir Ruby
+cd Ruby
+git clone https://github.com/digininja/CeWL.git
+cd CeWL
+gem install bundler
+bundle install
 mkdir Bash-Scripts/UserRecon
 cd Bash-Scripts/UserRecon/
 git clone https://github.com/wishihab/userrecon.git
