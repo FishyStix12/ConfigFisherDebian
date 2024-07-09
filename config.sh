6e5d441437eefc4dbea4730d97ad4079984ea264
Nicholas Fisher <102126354+FishyStix12@users.noreply.github.com>
2024-07-09 18:55:01 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -184,6 +184,7 @@ mv WHPython_v1.02-main/github_trojan.py ~/Python/Frame/Config
 mv WHPython_v1.02-main/WIns_overflow.py ~/Python/XFun
 mv WHPython_v1.02-main/WIns_OvrWrte.py ~/Python/XFun
 mv WHPython_v1.02-main/statrun.spk ~/Python/XFun
+mv WHPython_v1.02-main.fuzzywuzzy.py ~/Python/XFun
 mv WHPython_v1.02-main/keylogger.py ~/Python/Survey
 mv WHPython_v1.02-main/screenshotter.py ~/Python/Survey
 mv WHPython_v1.02-main/XtremeGame.py ~/Python/XFun
